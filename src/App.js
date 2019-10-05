145488e1abab0a84a6f3d2f7894e1bc123f6d583
Brian Han <bmhan319@hotmail.com>
2019-10-04 18:20:03 -0700

    added uv display interface

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import InputLocation from "./Components/InputLocation"
 import CurrentWeather from './Components/CurrentWeather'
 import Forecast from './Components/Forecast'
+import UVIndex from './Components/UVIndex'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <InputLocation />
         <CurrentWeather />
         <Forecast />
+        <UVIndex />
       </div>
     );
   }
